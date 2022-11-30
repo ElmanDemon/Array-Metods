2642ee9be0e4f60a329a86f601e782b5a11680f4
ElmanDemon <tu7lhkd9w@code.edu.az>
2022-12-01 02:16:33 +0400

    array tasks

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// // task1
+// let arr=[1,25,244,44,55,22,64]
+// const average=arr.reduce((a,b) => a + b, 0)/ arr.length;
+// console.log(average);
+
+// // task2
+// let arr=[1,5,2,7];
+// arr.push(11);
+// console.log(arr);
+
+// task3
